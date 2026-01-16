966e96c92857291f6c0d262305af88615cf3afc6
4judyyahmed <judya1316@gmail.com>
2026-01-16 09:53:31 +0800

    Add initial run_analysis.py for sales analysis

---
+++ run_analysis.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.py as mp
+import add_revenue from helpers.csv
+
+def=pd.load(sales.csv)
+
+list=products[0:0],units[1:0],price[2:0]
+graph=mp.[product,revenue]
+output=mp.list
+total_revenue=column(units*prices)
+print(output)
+print(graph)
